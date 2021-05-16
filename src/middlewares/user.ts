8952e565e8c7deaa77358866a096b4304bd7d925
ha-jinwoo <gkwlsdn95@naver.com>
2021-05-16 14:48:06 +0900

    Create user middleware userById isSameUser

---
+++ src/middlewares/user.ts
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+/* eslint-disable import/no-unresolved */
+import { Request, Response, NextFunction } from 'express';
+import { ObjectId } from 'mongoose';
+import UserModel from '../models/user';
+
+export const userById = async (req: Request, res: Response, next: NextFunction, userId:ObjectId) => {
+  try {
+    const user = await UserModel.findById({ userId });
+
+    if (!user) {
+      return res.status(400).json({
+        message: '유저가 존재하지 않습니다',
+      });
+    }
+
+    req.body.user = user;
+    return next();
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' });
+  }
+};
+
+export const isSameUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const isSame = req.body.user.email === req.body.authUser.email;
+
+    if (!isSame) {
+      return res.status(403).json({ message: '유저 권한이 없습니다' });
+    }
+
+    return next();
+  } catch (error) {
+    return res.status(500).json({ message: '서버 에러로 요청을 처리할 수 없습니다' });
+  }
+};
