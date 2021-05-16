b66eff0617cceb2338bc5f5026785bb02dc4efbe
Jace Lee <jarre9519@gmail.com>
2021-05-16 11:46:11 +0900

    Add location field to service model

--- src/models/service.ts
+++ src/models/service.ts
@@ -7,38 +7,50 @@ export interface Service {
   availableDays: string[],
   availableTimes: string[],
   greeting: string,
-  isDriver: boolean
+  isDriver: boolean,
+  location: string,
 }
 
 const ServiceSchema = new Schema<Service>({
   servedBy: {
-    type: String,
+    type: Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
     unique: true,
   },
   content: {
     type: String,
     trim: true,
+    required: true,
   },
   wage: {
     type: Number,
+    required: true,
   },
   availableDays: [
     {
       type: String,
+      required: true,
     },
   ],
   availableTimes: [
     {
       type: String,
+      required: true,
     },
   ],
   greeting: {
     type: String,
     trim: true,
+    required: true,
   },
   isDriver: {
     type: Boolean,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
   },
 });
 
