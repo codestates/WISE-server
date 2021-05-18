46facd414ea6ed801841ab99524a0b71f75722d3
ha-jinwoo <gkwlsdn95@naver.com>
2021-05-18 17:50:12 +0900

    Fix service images upload

--- src/routes/service.ts
+++ src/routes/service.ts
@@ -10,8 +10,16 @@ import { upload } from '../utils/s3';
 const router = Router();
 
 router.get('/services/:serviceId', getService);
-router.post('/services', auth, upload.array('images', 3), createService);
-router.patch('/services/:serviceId', auth, isSameAssistant, upload.array('images', 3), updateService);
+router.post('/services', auth, upload.fields([
+  { name: 'images', maxCount: 3 },
+  { name: 'trainingCert', maxCount: 3 },
+  { name: 'orgAuth', maxCount: 3 },
+]), createService);
+router.patch('/services/:serviceId', auth, isSameAssistant, upload.fields([
+  { name: 'images', maxCount: 3 },
+  { name: 'trainingCert', maxCount: 3 },
+  { name: 'orgAuth', maxCount: 3 },
+]), updateService);
 router.delete('/services/:serviceId', auth, isSameAssistant, deleteService);
 
 router.param('serviceId', serviceById);
