58476ca3c4c84ccf457e7e4c9a994c1cd3005c80
Jace Lee <jarre9519@gmail.com>
2021-05-19 17:11:15 +0900

    Fix auth logic

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import { Router } from 'express';
 import auth from '../middlewares/auth';
-import { signup, signin } from '../controllers/auth';
+import { validateEmail, signup, signin } from '../controllers/auth';
 
 const router = Router();
 
+router.post('/email-validation', validateEmail);
 router.post('/signup', auth, signup);
 router.post('/signin', auth, signin);
 
