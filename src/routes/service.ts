38fd9cfef7b4a564b101a44f284958f2efcaad4f
ha-jinwoo <gkwlsdn95@naver.com>
2021-05-24 19:22:10 +0900

    Fix service logic get all services and get services

--- src/routes/service.ts
+++ src/routes/service.ts
@@ -1,15 +1,17 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-unresolved */
 import { Router } from 'express';
 import auth from '../middlewares/auth';
 import { serviceById, isSameAssistant } from '../middlewares/service';
 import {
-  getService, createService, updateService, deleteService, getAllServices, getServices,
+  getService, createService, updateService, deleteService, getAllServices, getServices, getPopularity,
 } from '../controllers/service';
 import { upload } from '../utils/s3';
 
 const router = Router();
 
 router.get('/services/all', getAllServices);
+router.get('/services/popularity', getPopularity);
 router.get('/services/:serviceId', getService);
 router.get('/services', getServices);
 router.post('/services', auth, upload.fields([
