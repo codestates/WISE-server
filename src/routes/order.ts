46b171eab07fa5ba51d071f4547c8901701b34da
Jace Lee <jarre9519@gmail.com>
2021-05-25 11:48:26 +0900

    Change reservation to order

---
+++ src/routes/order.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-unresolved */
+import { Router } from 'express';
+import auth from '../middlewares/auth';
+import { orderById, isOrderUser } from '../middlewares/order';
+import { isSameUser, userById } from '../middlewares/user';
+
+import {
+  getOrders, createOrder, updateOrder, deleteOrder,
+} from '../controllers/order';
+
+const router = Router();
+
+router.get('/orders/:userId', auth, isSameUser, getOrders);
+router.post('/orders', auth, createOrder);
+router.patch('/orders/:orderId', auth, isOrderUser, updateOrder);
+router.delete('/orders/:orderId', auth, isOrderUser, deleteOrder);
+
+router.param('userId', userById);
+router.param('orderId', orderById);
+
+export default router;
