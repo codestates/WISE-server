7e947766dfe0ac496e16b0c358712ee2a3a3c494
ha-jinwoo <gkwlsdn95@naver.com>
2021-05-27 18:40:46 +0900

    Fix order logic and auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable import/no-unresolved */
 import { Response, NextFunction } from 'express';
 import admin from '../firebase';
@@ -5,15 +6,17 @@ import admin from '../firebase';
 const auth = async (req: any, res: Response, next: NextFunction) => {
   try {
     const accessToken = req.header('accessToken');
+    console.log(accessToken);
     if (!accessToken) {
       return res.status(400).json({ message: '로그인을 해주세요' });
     }
-    console.log(accessToken);
-    const authenticatedUser = await admin.auth().verifyIdToken(accessToken as string);
 
-    req.authUser = authenticatedUser;
-
-    return next();
+    admin.auth().verifyIdToken(accessToken as string).then((authenticatedUser) => {
+      req.authUser = authenticatedUser;
+      return next();
+    }).catch(() => res.status(400).json({
+      message: '로그인을 다시 해주세요',
+    }));
   } catch (error) {
     return res.status(500).json({
       message: '서버 에러로 요청을 처리할 수 없습니다',
